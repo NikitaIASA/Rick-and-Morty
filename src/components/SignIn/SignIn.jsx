ddd4d150a772641d9e42895da8528319ba14b46b
Nikita Chaika <iasastudent2021@gmail.com>
2023-03-16 18:57:03 +0200

    navigate fix

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,18 +1,26 @@
-import React from "react";
-import { UserAuth } from '../../context/AuthProvider';
+import React, {useEffect} from "react";
+import { useNavigate } from "react-router-dom";
 
+import { UserAuth } from '../../context/AuthProvider';
 import logo from "../../assets/img/logo.jpg";
 
 import classes from "./SignIn.module.scss";
 
 const Signin = () => {
   const { user } = UserAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user != null) {
+      navigate('/profile');
+    }
+  }, [user, navigate]);
 
   return (
     <div className={classes.wrap}>
       <img className={classes.logo} src={logo} alt="logo" />
       <h1 className={classes.title}>
-        {user ? "You are already logged in. To log out, use the button above" : "Sign in with your Google account"}
+        Sign in with your Google account
       </h1>
     </div>
   );
